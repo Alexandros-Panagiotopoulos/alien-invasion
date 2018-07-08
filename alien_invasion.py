a196b70499764a9705d74dd12af1fc6064e41a8e
Alexandros Panagiotopoulos <panagiotopoulos.alexandros@gmail.com>
2018-07-08 23:06:58 +0100

    add settings as a class and use an instance

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,13 +2,13 @@ import sys
 
 import pygame
 
+from settings import Settings
 def run_game():
-    #initialize game and create a screen object.
+    #initialize pygame, settings and screen object
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien_Invasion")
-    #set the backgraound color
-    bg_color = (230, 230, 230)
     #start the main loop for the game
     while True:
 
@@ -17,7 +17,7 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
         #redraw the screen during each pass through the loop.
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
         #Make the most recently drawn screen visible.
         pygame.display.flip()
 
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """A class to store all the Settings for Alien Invasion."""
+
+    def __init__(self):
+        """initialize the game's Settings"""
+        #screen Settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
