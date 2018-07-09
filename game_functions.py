662882fc83b6a97e2cd62733598b0a8f72a3dce3
Alexandros Panagiotopoulos <panagiotopoulos.alexandros@gmail.com>
2018-07-09 22:02:03 +0100

    add the shooting bullets posibility

--- game_functions.py
+++ game_functions.py
@@ -1,13 +1,18 @@
 import sys
 
 import pygame
+from bullet import Bullet
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Respond to keypresses"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        # Create a new bullet and add it to the bullet'g Group
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 def check_keyup_events(event, ship):
     """Respond to keyup"""
@@ -16,20 +21,23 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     """Respond to keypresses and mouse events"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     """update images on the screen and flip to the new screen"""
     #redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
+    # Redraw all bullets behind ship and aliens
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
     #Make the most recently drawn screen visible.
     pygame.display.flip()
